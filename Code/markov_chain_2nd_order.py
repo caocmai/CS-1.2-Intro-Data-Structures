007e7e242f4b76e5e7ab5012956012d5dfef2a17
Cao Mai <caomai@rocketmail.com>
2019-12-09 22:30:32 -0800

    [add] get sentence method added to 2nd order markov

--- Code/markov_chain_2nd_order.py
+++ Code/markov_chain_2nd_order.py
@@ -8,18 +8,27 @@ class Markov(dict):
     def __init__(self, content=None, order=1):
         
         self.types = 0
-        # self.tokens = 1
         self.order = order
         self.markov_chain(content, order)
 
     def markov_chain(self, content, order):
-
+    """Creates a markov chain with specified order"""
         for i in range(len(content) - order):
             if tuple(content[i: i + order]) not in self.keys():
                 self[tuple(content[i: i + order])] = []
                 self.types += 1
             self[tuple(content[i: i + order])].append(tuple(content[i + 1 : i + order + 1]))
-            # self.tokens += 1
         for key in self.keys():
             self[key] = Dictogram(self[key])
         
+    def get_sentence(self, length):
+    """Get a random sentence with the specified length"""
+        start = random.choice(list(self.keys()))
+        sentence = list(start)
+
+        for _ in range(length - self.order):
+            next_tuple = self[start].sample()
+            sentence.append(next_tuple[self.order - 1])
+            start = next_tuple
+
+        return ' '.join(sentence)
