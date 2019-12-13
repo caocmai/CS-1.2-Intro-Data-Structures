c6473f2b1936c0097bcfd1fda6fa54e77b3bce49
Cao Mai <caomai@rocketmail.com>
2019-12-12 16:30:17 -0800

    [add] more comments to nth order markov

--- Code/markov_chain_nth_order.py
+++ Code/markov_chain_nth_order.py
@@ -7,39 +7,38 @@ class Markov(dict):
 
     def __init__(self, content=None, order=1):
         
-        self.types = 0
         self.order = order
         self.markov_chain(content, order)
 
     def markov_chain(self, content, order):
         """Creates a markov chain with specified order"""
-        for i in range(len(content) - order):
+        for i in range(len(content) - order): # Loop through content minus the order
             if tuple(content[i: i+order]) not in self.keys(): # Markov is a dictionary
-                self[tuple(content[i: i+order])] = []
-                self.types += 1
+                self[tuple(content[i: i+order])] = [] # Set new tuple to be a list, if not in keys
 
-            self[tuple(content[i: i+order])].append(tuple(content[i+1 : i+order+1]))
+            self[tuple(content[i: i+order])].append(tuple(content[i+1 : i+order+1])) # else append the next word(s) based on the order
         
-        for key in self.keys():
-            self[key] = Dictogram(self[key])
+        for key in self.keys(): 
+            self[key] = Dictogram(self[key]) # Dictogram is assiged to self[key]
         
     def get_sentence(self, length):
         """Get a random sentence with the specified length"""
-        start = random.choice(list(self.keys()))
-        sentence = list(start)
+        first_word = random.choice(list(self.keys())) # Get random word
+        sentence = list(first_word) # List starts with first word
 
         for _ in range(length - self.order):
-            next_tuple = self[start].sample() # Use sample method from Dictogram class to get next
+            next_tuple = self[first_word].sample() # Use sample method from Dictogram class to get next
             sentence.append(next_tuple[self.order - 1])
-            start = next_tuple
+            first_word = next_tuple # To move down the chain
 
         return ' '.join(sentence)
 
 
 if __name__ == '__main__':
-    words = open_file("test3.txt")
+    # Testing 2nd order markov
+    content = open_file("test3.txt")
     # words = ['i', 'went', 'left', 'you', 'went', 'right', 'i','went','left', 'i', 'went', 'right']
-    markov_chain_2nd_order = Markov(words, 2)
+    markov_chain_2nd_order = Markov(content, 2)
     assert markov_chain_2nd_order == {
         ('i' , 'like'): {('like', 'cats'): 1, ('like', 'dogs'): 1}, 
         ('like', 'cats'): {('cats', 'and'): 1, ('cats', 'i'): 1},
--- Code/tweet_generator/markov_chain_nth_order.py
+++ Code/tweet_generator/markov_chain_nth_order.py
@@ -7,7 +7,6 @@ class Markov(dict):
 
     def __init__(self, content=None, order=1):
         
-        self.types = 0
         self.order = order
         self.markov_chain(content, order)
 
@@ -16,28 +15,30 @@ class Markov(dict):
         for i in range(len(content) - order):
             if tuple(content[i : i + order]) not in self.keys():
                 self[tuple(content[i : i + order])] = []
-                self.types += 1
+
             self[tuple(content[i : i + order])].append(tuple(content[i + 1 : i + order + 1]))
+
         for key in self.keys():
             self[key] = Dictogram(self[key])
         
     def get_sentence(self, length):
         """Get a random sentence with the specified length"""
-        start = random.choice(list(self.keys()))
-        sentence = list(start)
+        first_word = random.choice(list(self.keys()))
+        sentence = list(first_word)
 
         for _ in range(length - self.order):
-            next_tuple = self[start].sample()
+            next_tuple = self[first_word].sample()
             sentence.append(next_tuple[self.order - 1])
-            start = next_tuple
+            first_word = next_tuple
 
         return ' '.join(sentence)
 
 
 if __name__ == '__main__':
     # words = open_file("test3.txt")
-    words = ['i', 'like', 'cats', 'and', 'you', 'like', 'cats', 'i', 'like', 'dogs', 'but', 'you', 'hate', 'dogs']
-    markov_chain_2nd_order = Markov(words, 2)
+    ## Testing 2nd order markov
+    content = ['i', 'like', 'cats', 'and', 'you', 'like', 'cats', 'i', 'like', 'dogs', 'but', 'you', 'hate', 'dogs']
+    markov_chain_2nd_order = Markov(content, 2)
     assert markov_chain_2nd_order == {
         ('i' , 'like'): {('like', 'cats'): 1, ('like', 'dogs'): 1}, 
         ('like', 'cats'): {('cats', 'and'): 1, ('cats', 'i'): 1},
